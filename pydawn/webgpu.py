2d16ccb1a39f0a835646094ec65ee4da6073bd23
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2024-12-23 20:05:51 +0100

    dawn-python on pypi (#1)
    * pip install
    
    * Should be correct now
    
    * Readme update

--- pydawn/webgpu.py
+++ pydawn/webgpu.py
@@ -145,8 +145,8 @@ def char_pointer_cast(string, encoding='utf-8'):
 
 
 _libraries = {}
-root_project_path = Path(__file__).resolve().parent.parent
-dll_path = root_project_path / 'resources' / 'libwebgpu_dawn.dylib'
+root_project_path = Path(__file__).resolve().parent
+dll_path = root_project_path / 'lib' / 'libwebgpu_dawn.dylib'
 _libraries['libwebgpu_dawn.dylib'] = ctypes.CDLL(str(dll_path))
 
 
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+long_description = Path("README.md").read_text()
+
+setup(
+    name="dawn-python",
+    version="0.1.0",
+    author="Ahmed Harmouche",
+    author_email="ahmedharmouche92@gmail.com",
+    description="A Python interface for WebGPU using Dawn.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wpmed92/pydawn",
+    packages=find_packages(),
+    platforms="macOS",
+    package_data={
+        "pydawn": ["lib/libwebgpu_dawn.dylib"]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS",
+        "License :: OSI Approved :: BSD License",
+    ],
+    python_requires=">=3.7",
+    install_requires=[],
+)
