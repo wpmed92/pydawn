7b2e2535114d432f7189525ce9aaceddbbb7a514
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2024-12-24 23:18:04 +0100

    Run some unit tests (#2)

---
+++ test/test_utils.py
@@ -0,0 +1,46 @@
+import unittest
+from pydawn import utils, webgpu
+
+class TestUtils(unittest.TestCase):
+    def setUp(self):
+        self.adapter = utils.request_adapter_sync(power_preference=webgpu.WGPUPowerPreference_HighPerformance)
+        self.assertIsNotNone(self.adapter, "adapter should not be None")
+        self.device = utils.request_device_sync(self.adapter, [webgpu.WGPUFeatureName_ShaderF16])
+        self.assertIsNotNone(self.device, "device should not be None")
+
+    def test_create_buffer(self):
+        size = 16
+        usage = webgpu.WGPUBufferUsage_Storage
+        buf = utils.create_buffer(self.device, size, webgpu.WGPUBufferUsage_Storage)
+        actual_size = webgpu.wgpuBufferGetSize(buf)
+        actual_usage = webgpu.wgpuBufferGetUsage(buf)
+        self.assertEqual(actual_size, size, f"unexpected buffer size: (actual) {actual_size} != (expected) {size}")
+        self.assertEqual(actual_usage, usage, f"unexpected buffer usage: (actual) {actual_usage} != (expected) {usage}")
+
+    def test_buffer_read_write(self):
+        buf = utils.create_buffer(self.device, 4, webgpu.WGPUBufferUsage_Storage | webgpu.WGPUBufferUsage_CopySrc | webgpu.WGPUBufferUsage_CopyDst)
+        test_src = [1, 2, 3, 4]
+        utils.write_buffer(self.device, buf, 0, test_src)
+        out = utils.read_buffer(self.device, buf)
+        self.assertEqual(list(out), test_src, f"buffer mismatch: (actual) {test_src} != (expected) {out}")
+
+    def test_create_shader_module(self):
+        shader_source = """
+            @group(0) @binding(0)
+            var<storage,read> data1: array<f32>;
+
+            @group(0) @binding(1)
+            var<storage,read_write> data2: array<f32>;
+
+            @compute
+            @workgroup_size(1)
+            fn main(@builtin(global_invocation_id) index: vec3<u32>) {
+                let i: u32 = index.x;
+                data2[i] = data1[i];
+            }
+        """
+        shader_module = utils.create_shader_module(self.device, shader_source)
+        self.assertIsNotNone(shader_module, "shader_module should not be None")
+
+if __name__ == "__main__":
+    unittest.main()
