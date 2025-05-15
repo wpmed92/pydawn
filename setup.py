128cb6b4181175939102a358aa3cddc5fcc50e5a
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2025-05-15 11:12:49 +0200

    Release v0.2.0 (#12)
    * Release v0.2.0
    
    * Include dawn dll

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = Path("README.md").read_text()
 
 setup(
     name="dawn-python",
-    version="0.1.7",
+    version="0.2.0",
     author="Ahmed Harmouche",
     author_email="ahmedharmouche92@gmail.com",
     description="A Python interface for the Dawn WebGPU engine.",
@@ -13,7 +13,12 @@ setup(
     url="https://github.com/wpmed92/pydawn",
     packages=find_packages(),
     platforms="macOS",
-    package_data={"pydawn": ["lib/libwebgpu_dawn.so", "lib/libwebgpu_dawn_arm.dylib", "lib/libwebgpu_dawn_x86_64.dylib"]},
+    package_data={"pydawn": [
+        "lib/libwebgpu_dawn.so", 
+        "lib/libwebgpu_dawn_arm.dylib", 
+        "lib/libwebgpu_dawn_x86_64.dylib",
+        "lib/libwebgpu_dawn.dll",
+    ]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: MacOS",
